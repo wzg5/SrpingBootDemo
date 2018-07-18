e69abeaa32eb658d0730d8744d0e967079aa564b
WangZG <wangzhigang@bonc.com.cn>
2018-07-18 10:20:35 +0800

    44444

--- src/main/java/com/example/demo/SrpingBootDemoApplication.java
+++ src/main/java/com/example/demo/SrpingBootDemoApplication.java
@@ -10,6 +10,7 @@ public class SrpingBootDemoApplication {
 		System.out.println("1111111");
 		System.out.println("2222222");
 		System.out.println("3333333");
+		System.out.println("4444444");
 		SpringApplication.run(SrpingBootDemoApplication.class, args);
 		
 	}
